12faa85612a04795b92840a087a7923d857779c5
bhaskar <bhaskar19n@gmail.com>
2026-03-04 21:45:11 +0530

    Authorization Token paramtermization class added Role ENUM constant ENUM is dded

--- src/test/java/com/phoenix/api/tests/fdUserDetailsAPITest.java
+++ src/test/java/com/phoenix/api/tests/fdUserDetailsAPITest.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
+import static com.api.utils.AuthTokenProvider.*;
 import com.api.utils.ConfigManager;
 import com.phoenix.api.pojo.loginUserDetails;
 
+import Constants.Role;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import io.restassured.module.jsv.JsonSchemaValidator;
@@ -19,7 +21,10 @@ import static com.api.utils.ConfigManager14.*;
 public class fdUserDetailsAPITest {
 	// RestAssured
 	//ConfigManager configManager= new ConfigManager();--Accessing  static method from ConfigManager class
-	Header authToken= new Header("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RfbmFtZSI6ImZkIiwibGFzdF9uYW1lIjoiZmQiLCJsb2dpbl9pZCI6ImlhbWZkIiwibW9iaWxlX251bWJlciI6Ijg4OTk3NzY2NTUiLCJlbWFpbF9pZCI6Im1hcmtAZ21haWwuY29tIiwicGFzc3dvcmQiOiI1ZjRkY2MzYjVhYTc2NWQ2MWQ4MzI3ZGViODgyY2Y5OSIsInJlc2V0X3Bhc3N3b3JkX2RhdGUiOm51bGwsImxvY2tfc3RhdHVzIjowLCJpc19hY3RpdmUiOjEsIm1zdF9yb2xlX2lkIjo1LCJtc3Rfc2VydmljZV9sb2NhdGlvbl9pZCI6MSwiY3JlYXRlZF9hdCI6IjIwMjEtMTEtMDNUMDg6MDY6MjMuMDAwWiIsIm1vZGlmaWVkX2F0IjoiMjAyMS0xMS0wM1QwODowNjoyMy4wMDBaIiwicm9sZV9uYW1lIjoiRnJvbnREZXNrIiwic2VydmljZV9sb2NhdGlvbiI6IlNlcnZpY2UgQ2VudGVyIEEiLCJpYXQiOjE3Njg5NzAyODJ9.QGGWq9PKYt1vHfzQ63RW9CyfoC95u0Qt0U7gVzAcdnk");
+	//Header authToken= new Header("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RfbmFtZSI6ImZkIiwibGFzdF9uYW1lIjoiZmQiLCJsb2dpbl9pZCI6ImlhbWZkIiwibW9iaWxlX251bWJlciI6Ijg4OTk3NzY2NTUiLCJlbWFpbF9pZCI6Im1hcmtAZ21haWwuY29tIiwicGFzc3dvcmQiOiI1ZjRkY2MzYjVhYTc2NWQ2MWQ4MzI3ZGViODgyY2Y5OSIsInJlc2V0X3Bhc3N3b3JkX2RhdGUiOm51bGwsImxvY2tfc3RhdHVzIjowLCJpc19hY3RpdmUiOjEsIm1zdF9yb2xlX2lkIjo1LCJtc3Rfc2VydmljZV9sb2NhdGlvbl9pZCI6MSwiY3JlYXRlZF9hdCI6IjIwMjEtMTEtMDNUMDg6MDY6MjMuMDAwWiIsIm1vZGlmaWVkX2F0IjoiMjAyMS0xMS0wM1QwODowNjoyMy4wMDBaIiwicm9sZV9uYW1lIjoiRnJvbnREZXNrIiwic2VydmljZV9sb2NhdGlvbiI6IlNlcnZpY2UgQ2VudGVyIEEiLCJpYXQiOjE3Njg5NzAyODJ9.QGGWq9PKYt1vHfzQ63RW9CyfoC95u0Qt0U7gVzAcdnk");
+	//Header authToken= new Header("Authorization",AuthTokenProvider.getToken());
+	Header authToken= new Header("Authorization",getToken(Role.FD));//becoz static import of class
+	
 	loginUserDetails userCredential= new loginUserDetails("iamfd", "password");
 	public void fdUserDetailsAPITest()  {
 		int userID= given()
