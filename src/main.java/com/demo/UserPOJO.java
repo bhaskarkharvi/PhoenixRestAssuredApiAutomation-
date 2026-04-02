2102930485807b336fdb9b1b5acb2a2c75448c91
bhaskar <bhaskar19n@gmail.com>
2026-04-02 17:35:29 +0530

    CsvToBean<UserPOJO>CSV data mapped to User credential data UserPOJO class

---
+++ src/main.java/com/demo/UserPOJO.java
@@ -0,0 +1,41 @@
+package com.demo;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class UserPOJO {
+	//private String username;
+	//private String password;
+	
+	@CsvBindByName(column="username") 
+	private String x;
+	
+	@CsvBindByName(column="password") 
+	private String y;
+	
+	
+	public UserPOJO() {
+		
+	}
+	public UserPOJO(String username , String password) {
+		this.x=username;
+		this.y=password;
+	
+	}
+	public String getUsername() {
+		return y;
+	}
+	public void setUsername(String username) {
+		this.y = username;
+	}
+	public String getPassword() {
+		return x;
+	}
+	public void setPassword(String password) {
+		this.x = password;
+	}
+	@Override
+	public String toString() {
+		return "UserPOJO [username=" + y + ", password=" + x + "]";
+	}
+	
+}
