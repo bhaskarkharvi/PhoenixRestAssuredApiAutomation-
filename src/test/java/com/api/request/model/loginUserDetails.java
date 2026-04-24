986a52966f4a231d1aa2a27398f97e2559d0550f
bhaskar <bhaskar19n@gmail.com>
2026-04-24 17:19:02 +0530

    Json User Login data used as DataProvider for LoginAPIJSONDataDrivenTest class

--- src/test/java/com/api/request/model/loginUserDetails.java
+++ src/test/java/com/api/request/model/loginUserDetails.java
@@ -4,6 +4,17 @@ public class loginUserDetails {
 	
 	private String username;
 	private String password;
+	
+	
+	public loginUserDetails() {
+    }
+	
+	//This default constructor is created because used in ObjectMappeDemo class readervalue(). So it was generating exception
+	//Exception in thread "main" com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `com.api.request.model.loginUserDetails` (no Creators, like default constructor, exist): 
+	//Above exception says that no default constructor in loginUserDetails class ,after creating a default constructor it worked.
+
+	
+	
 	public loginUserDetails(String username, String password) {
 		super();
 		this.username = username;
